2c1321419fd2b7d9b759c786432a08059030f7a4
Hristo Damyanov <hristo007@gmail.com>
2025-01-14 09:21:34 +0200

    update

--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,3 @@
-let API_URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
 let API_KEY = '2663a47da530c754207396eef5db5fb1';
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -15,15 +14,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 function fetchMovie(title) {
-    fetch(BASE_URL + `t=${title}`)
-        .then(response => response.json)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${title}`)
+        .then(response => response.json())
         .then(data => {
-            const movie = data.result[0];
+            const movie = data.results[0];
+            console.log(movie);
             document.getElementById('title').textContent = movie.title;
             document.getElementById('overview').textContent = movie.overview;
-            document.getElementById('release-date').textContent = movie.release_date
-            document.getElementById('vote-average').textContent = movie.vote_average;
-            document.getElemetnById('original-language').textContent = movie.original_language;
-            document.getElementById('poster').src = `https://image.tmdb.org/t/p/w500`
+            document.getElementById('release_date').textContent = movie.release_date;
+            document.getElementById('vote_average').textContent = movie.vote_average;
+            document.getElementById('original_language').textContent = movie.original_language;
+            document.getElementById('poster').src = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
         })
+        .catch(error => console.error(error));
 }
